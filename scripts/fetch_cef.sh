1a0afa882aa262d95d795ae1040d76c725082899
lowpolyphosphorus <158785409+LowPolyPhosphorus@users.noreply.github.com>
2026-05-09 21:44:22 -0700

    Update CEF version in fetch_cef.sh script

--- scripts/fetch_cef.sh
+++ scripts/fetch_cef.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-CEF_VERSION="137.0.10+g7e14fe1+chromium-137.0.7151.69"
+CEF_VERSION="144.0.6+g5f7e671+chromium-144.0.7559.59"
 PLATFORM="linux64"
 FILENAME="cef_binary_${CEF_VERSION}_${PLATFORM}_minimal.tar.bz2"
 URL="https://cef-builds.spotifycdn.com/${FILENAME}"
 
-echo "Downloading CEF from: $URL"
+echo "Downloading CEF $CEF_VERSION..."
 curl -L --fail --show-error "$URL" -o cef.tar.bz2
 
 echo "Extracting..."
@@ -14,5 +14,5 @@ mkdir -p cef
 tar -xj --strip-components=1 -C cef -f cef.tar.bz2
 rm cef.tar.bz2
 
-echo "Done! CEF contents:"
+echo "Done!"
 ls cef/
